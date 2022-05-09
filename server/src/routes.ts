87f5f2edd3b0b77dbcfb6ba802090b3ca643057e
enzodmrib <enzorib2@gmail.com>
2022-05-09 03:19:39 -0300

    Adds try catch on route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -7,21 +7,25 @@ export const routes = express.Router();
 
 routes.post('/feedbacks', async (req, res) => {
     const { type, comment, screenshot } = req.body;
+    try {
+        const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+        const nodemailerMailAdapter = new NodemailerMailAdapter();
 
-    const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
-    const nodemailerMailAdapter = new NodemailerMailAdapter();
-    
-    
-    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-        prismaFeedbacksRepository,
-        nodemailerMailAdapter
-    );
 
-    await submitFeedbackUseCase.execute({
-        type, 
-        comment, 
-        screenshot
-    });
+        const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+            prismaFeedbacksRepository,
+            nodemailerMailAdapter
+        );
 
-    return res.status(201).send()
+        await submitFeedbackUseCase.execute({
+            type,
+            comment,
+            screenshot
+        });
+
+        return res.status(201).send()
+
+    } catch (error) {
+        return res.status(500).send()
+    }
 })
